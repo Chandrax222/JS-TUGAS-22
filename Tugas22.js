39054c59e801a03d394eae906da0908754a4f4e3
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-30 12:06:20 +0700

    foreach item, index dan array
    foreach item, index dan array

---
+++ Tugas22.js
@@ -0,0 +1,10 @@
+
+    function Tugas22() {
+        var isi = ["Saya", "ingin", "belajar", "bersama"];
+        isi.forEach(function(item,index,array) {
+            console.log("Item : "+item+" index ke "+index);
+        });
+    }
+    Tugas22();
+
+    
